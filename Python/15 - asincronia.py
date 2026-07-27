e7d7e0c0a533b1a2fa288b9d7ba36c0ad5d9c64a
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-07-27 15:06:27 -0500

    Agregar archivo que implementa funcionalidades de asincronía en Python, incluyendo tareas concurrentes y manejo de tiempos de ejecución.

---
+++ Python/15 - asincronia.py
@@ -0,0 +1,28 @@
+import datetime
+import asyncio
+
+
+"""
+Asincronia
+"""
+
+async def task(name: str, duration: int):
+    print(
+        f"Tarea: {name}, Duración: {duration}s. Inicio: {datetime.datetime.now()}")
+    await asyncio.sleep(duration)
+    print(
+        f"Tarea: {name}. Fin: {datetime.datetime.now()}")
+
+
+    asyncio.run(task("1", 2))
+
+"""
+Extra
+"""
+
+
+async def async_task():
+    await asyncio.gather(task("C", 3), task("B", 2), task("A", 1))
+    await task("D", 1)
+
+asyncio.run(async_task())
